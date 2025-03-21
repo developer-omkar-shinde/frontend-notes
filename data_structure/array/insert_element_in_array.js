b4a03e8c1aed2a7f6d2770f2d66daed76ffc88eb
omkars <oshinde796@gmail.com>
2025-03-21 13:27:53 +0530

    insert ele in array

---
+++ data_structure/array/insert_element_in_array.js
@@ -0,0 +1,28 @@
+// without .splice()
+
+const insertInArray = (array, position, newElement) => {
+  if (position < 0 || position > array.length) {
+    throw new Error("Invalid position");
+  }
+
+  const data = [...array];
+
+  for (let i = data.length - 1; i >= 0; i--) {
+    const currentEle = data[i];
+    console.log("ele", currentEle);
+    console.log("index", i);
+
+    if (i >= position) {
+      data[i + 1] = data[i];
+      if (position === i) {
+        data[i] = newElement;
+      }
+    }
+  }
+
+  return data;
+};
+
+const result = insertInArray([11, 23, 44, 25, 65], 2, 100);
+
+console.warn("result", result);
