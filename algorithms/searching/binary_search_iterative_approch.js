e01a10cd151faf3d702e283e3d2f4f6ae9be1905
omkars <oshinde796@gmail.com>
2025-03-26 13:51:55 +0530

    binary_search_iterative_approch

---
+++ algorithms/searching/binary_search_iterative_approch.js
@@ -0,0 +1,24 @@
+// Only works on sorted Array.
+
+const binarySearch = (target, array) => {
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (array[mid] === target) {
+      return mid;
+      break;
+    } else if (target < array[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+};
+
+const index = binarySearch(19, [1, 2, 3, 4, 5, 6, 7, 8]);
+
+console.log(index);
